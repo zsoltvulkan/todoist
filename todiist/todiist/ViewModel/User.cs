9efb000e70c8ad46abfb272a5a4acb3cc9bded8b
VulkanZsolt <zsoltvulkan@gmail.com>
2023-02-04 22:29:16 +0200

    Compose file and database first table with inseted values

---
+++ todiist/todiist/ViewModel/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace todiist.ViewModel
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ todiist/todiist/db/Init.sql
@@ -0,0 +1,10 @@
+﻿
+CREATE TABLE IF NOT EXISTS `felhasznalok` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) DEFAULT NULL,
+  `password` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=MySQL  DEFAULT CHARSET=UTF-8 AUTO_INCREMENT=10001 ;
+
+INSERT INTO `users` (`user_id`, `username`,  `password`) VALUES
+(1, 'zsolt', 'admin');
